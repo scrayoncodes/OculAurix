65b31645ce0a964793b02feade36eb43e4a9cabc
scrayoncodes <143898937+scrayoncodes@users.noreply.github.com>
2025-08-26 08:40:44 -0400

    creation of main.py, README.md, and .gitignore

---
+++ TSA_Software_Development/main.py
@@ -0,0 +1,29 @@
+#
+import sys;
+# 
+from PyQt6.QtWidgets import (
+    QApplication, #
+    QWidget, #
+    QPushButton, #
+    QVBoxLayout, # 
+    QLineEdit, #
+    QLabel, #
+    QSplashScreen #
+)
+# 
+from PyQt6.QtGui import(
+    QFont, #
+    QPixmap, #
+)
+# 
+from PyQt6.QtCore import(
+    Qt, #
+    QTimer, #
+    QCoreApplication, #
+)
+
+# 
+import pyttsx3 # 
+
+# 
+App_Name = "OculAurix" 
